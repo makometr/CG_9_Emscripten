dc6f12c61dfd3a652522c0ffcb160280541325f6
makometr <makometr@yandex.ru>
2020-11-20 15:12:59 +0300

    Final changes for scene

--- source/main.cpp
+++ source/main.cpp
@@ -74,7 +74,7 @@ class App : public BaseApp {
 	GLfloat lastFrame = 0.0f;
 
 	// Cube state
-	float cubeRotateSpeed = 1;
+	float cubeRotateSpeed = 0;
 	glm::vec3 cubePosition {0.0f};
 	glm::vec3 cubeRotate {0.0f};
 	GLfloat cubeRotateValue {0.0f};
@@ -93,8 +93,8 @@ class App : public BaseApp {
 	unsigned specularTexture;
 
 	void Start() override {
-		auto texture_load_result_1 = std::async(std::launch::deferred, TextureLoader::loadTexture, "resources/textures/container2.png");
-		auto texture_load_result_2 = std::async(std::launch::deferred, TextureLoader::loadTexture, "resources/textures/container2_specular.png");
+		auto texture_load_result_1 = std::async(std::launch::deferred,TextureLoader::loadTexture, "resources/textures/container2.png");
+		auto texture_load_result_2 = std::async(std::launch::deferred,TextureLoader::loadTexture, "resources/textures/container2_specular.png");
 		glEnable(GL_DEPTH_TEST);
 
 		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
@@ -140,7 +140,7 @@ class App : public BaseApp {
 		ImGui::End();
 
 
-		ImGui::SetNextWindowSize({300, 130}, ImGuiCond_Once);
+		ImGui::SetNextWindowSize({300, 150}, ImGuiCond_Once);
 		ImGui::SetNextWindowPos({10, 100}, ImGuiCond_Once);
 		ImGui::SetNextWindowCollapsed(false, ImGuiCond_Once);
 		ImGui::Begin("Light-1");
@@ -151,8 +151,8 @@ class App : public BaseApp {
 			ImGui::Checkbox("Light enabled", &pointLightsTurned[0]);
 		ImGui::End();
 
-		ImGui::SetNextWindowSize({300, 130}, ImGuiCond_Once);
-		ImGui::SetNextWindowPos({10, 240}, ImGuiCond_Once);
+		ImGui::SetNextWindowSize({300, 150}, ImGuiCond_Once);
+		ImGui::SetNextWindowPos({10, 260}, ImGuiCond_Once);
 		ImGui::SetNextWindowCollapsed(false, ImGuiCond_Once);
 		ImGui::Begin("Light-2");
 			ImGui::SliderFloat("Translate: X", &pointLightPosition_2.x, -10.0, 10.0);
@@ -162,30 +162,30 @@ class App : public BaseApp {
 			ImGui::Checkbox("Light enabled", &pointLightsTurned[1]);
 		ImGui::End();
 
-		ImGui::SetNextWindowSize({300, 170}, ImGuiCond_Once);
-		ImGui::SetNextWindowPos({10, 380}, ImGuiCond_Once);
-		ImGui::SetNextWindowCollapsed(false, ImGuiCond_Once);
-		ImGui::Begin("Cube");
-			ImGui::SliderFloat("Speed", &cubeRotateSpeed, -100.0, 100.0);
-			// ImGui::SliderFloat("Translate: X", &cubePosition.x, -10.0, 10.0);
-			// ImGui::SliderFloat("Translate: Y", &cubePosition.y, -10.0, 10.0);
-			// ImGui::SliderFloat("Translate: Z", &cubePosition.z, -10.0, 10.0);
-			ImGui::SliderFloat("X Rotate", &cubeRotate.x, 0.0, 360.0);
-			ImGui::SliderFloat("Y Rotate", &cubeRotate.y, 0.0, 360.0);
-			ImGui::SliderFloat("Z Rotate", &cubeRotate.z, 0.0, 360.0);
-			ImGui::SliderFloat("Scale", &cubeScale, -5.0, 5.0);
-			static int materialType = 0;
-			ImGui::RadioButton("Gold", &materialType, 0); ImGui::SameLine();
-			ImGui::RadioButton("Cyan Plastic", &materialType, 1); ImGui::SameLine();
-			ImGui::RadioButton("Emerlad", &materialType, 2);
-		ImGui::End();
+		// ImGui::SetNextWindowSize({300, 170}, ImGuiCond_Once);
+		// ImGui::SetNextWindowPos({10, 380}, ImGuiCond_Once);
+		// ImGui::SetNextWindowCollapsed(false, ImGuiCond_Once);
+		// ImGui::Begin("Cube");
+		// 	// ImGui::SliderFloat("Speed", &cubeRotateSpeed, -100.0, 100.0);
+		// 	// ImGui::SliderFloat("Translate: X", &cubePosition.x, -10.0, 10.0);
+		// 	// ImGui::SliderFloat("Translate: Y", &cubePosition.y, -10.0, 10.0);
+		// 	// ImGui::SliderFloat("Translate: Z", &cubePosition.z, -10.0, 10.0);
+		// 	ImGui::SliderFloat("X Rotate", &cubeRotate.x, 0.0, 360.0);
+		// 	ImGui::SliderFloat("Y Rotate", &cubeRotate.y, 0.0, 360.0);
+		// 	ImGui::SliderFloat("Z Rotate", &cubeRotate.z, 0.0, 360.0);
+		// 	ImGui::SliderFloat("Scale", &cubeScale, -5.0, 5.0);
+		// 	static int materialType = 0;
+		// 	ImGui::RadioButton("Gold", &materialType, 0); ImGui::SameLine();
+		// 	ImGui::RadioButton("Cyan Plastic", &materialType, 1); ImGui::SameLine();
+		// 	ImGui::RadioButton("Emerlad", &materialType, 2);
+		// ImGui::End();
 
-		if (materialType == 0)
-			curMat = materialManager.getMaterial(MaterialType::GOLD);
-		if (materialType == 1)
-			curMat = materialManager.getMaterial(MaterialType::cyanPlastic);
-		if (materialType == 2)
-			curMat = materialManager.getMaterial(MaterialType::Emerlad);
+		// if (materialType == 0)
+		// 	curMat = materialManager.getMaterial(MaterialType::GOLD);
+		// if (materialType == 1)
+		// 	curMat = materialManager.getMaterial(MaterialType::cyanPlastic);
+		// if (materialType == 2)
+		// 	curMat = materialManager.getMaterial(MaterialType::Emerlad);
 
         glm::mat4 view = camera.GetViewMatrix();
 
@@ -225,7 +225,7 @@ class App : public BaseApp {
 			model = glm::rotate(model, glm::radians(rotate.x), glm::vec3(1.0, 0.0, 0.0));
 			model = glm::rotate(model, glm::radians(rotate.y), glm::vec3(0.0, 1.0, 0.0));
 			model = glm::rotate(model, glm::radians(rotate.z), glm::vec3(0.0, 0.0, 1.0));
-			model = glm::scale(model, glm::vec3(scale));
+			model = glm::scale(model, glm::vec3(1.0f, 3.0f, 1.0f));
 
 			// glm::mat4 transformMatrix = projection * view * model;
 			shaderProg.set("model", model);
@@ -239,9 +239,7 @@ class App : public BaseApp {
 			// shaderProg.set("material", curMat.get());
 			shaderProg.set("material.diffuse", 0);
 			shaderProg.set("material.specular", 1);
-			// shaderProg.set("material.specular", {0.5f, 0.5f, 0.5f});
     		shaderProg.set("material.shininess", 64.0f);
-
 
 
 			shaderProg.set("pointLights[0].position", lightPos_1);
@@ -249,23 +247,68 @@ class App : public BaseApp {
 			shaderProg.set("pointLights[0].diffuse", plColor_1 * glm::vec3{1.0f, 1.0f, 1.0f});
 			shaderProg.set("pointLights[0].specular", plColor_1 * glm::vec3{1.0f, 1.0f, 1.0f});
 			shaderProg.set("pointLights[0].constant", 1.0f);
-			shaderProg.set("pointLights[0].linear", 0.027f);  
-			shaderProg.set("pointLights[0].quadratic", 0.0028f);
+			shaderProg.set("pointLights[0].linear", 0.14f);  
+			shaderProg.set("pointLights[0].quadratic", 0.07f);
 
 			shaderProg.set("pointLights[1].position", lightPos_2);
 			shaderProg.set("pointLights[1].ambient", plColor_2 * glm::vec3{1.0f, 1.0f, 1.0f});
 			shaderProg.set("pointLights[1].diffuse", plColor_2 * glm::vec3{1.0f, 1.0f, 1.0f});
 			shaderProg.set("pointLights[1].specular", plColor_2 * glm::vec3{1.0f, 1.0f, 1.0f});
 			shaderProg.set("pointLights[1].constant", 1.0f);
-			shaderProg.set("pointLights[1].linear", 0.027f);
-			shaderProg.set("pointLights[1].quadratic", 0.0028f);
+			shaderProg.set("pointLights[1].linear", 0.14f);
+			shaderProg.set("pointLights[1].quadratic", 0.07f);
 
 
 			glActiveTexture(GL_TEXTURE0);
 			glBindTexture(GL_TEXTURE_2D, diffTexture);
 			glActiveTexture(GL_TEXTURE1);
 			glBindTexture(GL_TEXTURE_2D, specTexture);
-		});  
+		});
+
+		figure_cube.draw(lightedTexturedObjectShader, [&projection, &view, rotate=cubeRotate, cameraPos=camera.Position, pos=cubePosition, scale=cubeScale, lightPos_1=pointLightPosition_1, lightPos_2=pointLightPosition_2, plColor_1=pointLightColor_1, plColor_2=pointLightColor_2, diffTexture=diffuseTexture, specTexture=specularTexture] (const Shader& shaderProg) {
+			glm::mat4 model {1.0f}; 
+			model = glm::translate(model, glm::vec3(pos.x+1.5f, pos.y-1.0f, pos.z));
+			model = glm::rotate(model, glm::radians(rotate.x), glm::vec3(1.0, 0.0, 0.0));
+			model = glm::rotate(model, glm::radians(rotate.y), glm::vec3(0.0, 1.0, 0.0));
+			model = glm::rotate(model, glm::radians(rotate.z), glm::vec3(0.0, 0.0, 1.0));
+			model = glm::scale(model, glm::vec3(2.0f, 1.0f, 1.0f));
+
+			// glm::mat4 transformMatrix = projection * view * model;
+			shaderProg.set("model", model);
+			shaderProg.set("view", view);
+			shaderProg.set("projection", projection);
+			// shaderProg.set("transform", transformMatrix);
+			// shaderProg.set("objectColor", glm::vec3(102.0f/256.0f, 1.0f, 1.0f));
+			shaderProg.set("pointLightsTurned[0]", pointLightsTurned[0]);
+			shaderProg.set("pointLightsTurned[1]", pointLightsTurned[1]);
+
+			shaderProg.set("material.diffuse", 0);
+			shaderProg.set("material.specular", 1);
+    		shaderProg.set("material.shininess", 64.0f);
+
+
+			shaderProg.set("pointLights[0].position", lightPos_1);
+			shaderProg.set("pointLights[0].ambient", plColor_1 * glm::vec3{1.0f, 1.0f, 1.0f});
+			shaderProg.set("pointLights[0].diffuse", plColor_1 * glm::vec3{1.0f, 1.0f, 1.0f});
+			shaderProg.set("pointLights[0].specular", plColor_1 * glm::vec3{1.0f, 1.0f, 1.0f});
+			shaderProg.set("pointLights[0].constant", 1.0f);
+			shaderProg.set("pointLights[0].linear", 0.14f);
+			shaderProg.set("pointLights[0].quadratic", 0.07f);
+
+			shaderProg.set("pointLights[1].position", lightPos_2);
+			shaderProg.set("pointLights[1].ambient", plColor_2 * glm::vec3{1.0f, 1.0f, 1.0f});
+			shaderProg.set("pointLights[1].diffuse", plColor_2 * glm::vec3{1.0f, 1.0f, 1.0f});
+			shaderProg.set("pointLights[1].specular", plColor_2 * glm::vec3{1.0f, 1.0f, 1.0f});
+			shaderProg.set("pointLights[1].constant", 1.0f);
+			shaderProg.set("pointLights[1].linear", 0.14f);
+			shaderProg.set("pointLights[1].quadratic", 0.07f);
+
+
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, diffTexture);
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, specTexture);
+		});
 
 		lightCube.draw(pointLightShader, [&projection, &view, pos=pointLightPosition_1, lightColor=pointLightColor_1] (const Shader& shaderProg) {
 			glm::mat4 model {1.0f};
